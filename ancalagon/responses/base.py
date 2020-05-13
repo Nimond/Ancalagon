cfa41f8cfd303f68fb4c21d972ec4f7a69c632be
Nimond <a.zhiltsov@ongrid.pro>
2020-05-13 20:25:11 +0300

    Add headers to response

--- ancalagon/responses/base.py
+++ ancalagon/responses/base.py
@@ -1,9 +1,13 @@
+from multidict import CIMultiDict
+
+
 class Response:
     content_type = b'text/html'
 
     def __init__(self, body='', status_code=200):
         self.body = body
         self.status_code = status_code
+        self.headers = CIMultiDict()
 
     async def __call__(self, scope, receive, send):
         body = self.render_body()
@@ -11,7 +15,14 @@ class Response:
             {
                 "type": "http.response.start",
                 "status": self.status_code,
-                "headers": [[b"content-type", self.content_type]],
+                "headers": [
+                    [b'content-type', self.content_type],
+                    [b'server', b'Ancalagon'],
+                ]
+                + [
+                    [key.encode('utf-8'), value.encode('utf-8')]
+                    for key, value in self.headers.items()
+                ],
             }
         )
         await send({"type": "http.response.body", "body": body})
